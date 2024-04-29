1e36fc56d959fb5eb8fcc2de09fb0e4342e32901
jijijiwon <ljiwon@gmail.com>
2024-04-29 09:05:35 +0900

    Fix_Mkefile

--- stop_server.sh
+++ stop_server.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-python=$(ps -ef | grep 'python')
-nodejs=$(ps -ef | grep 'node')
-json=$(ps -ef | grep 'json-server')
+python=$(netstat -ntlp | grep 3000)
+nodejs=$(netstat -ntlp | grep 8000)
+json=$(netstat -ntlp | grep 5000)
 
-second1=$(echo ${python} | cut -d " " -f2)    
-second2=$(echo ${nodejs} | cut -d " " -f2)
-second3=$(echo ${json} | cut -d " " -f2)
+second1=$(echo ${python} | cut -d " " -f7 | cut -d "/" -f1)    
+second2=$(echo ${nodejs} | cut -d " " -f7 | cut -d "/" -f1)
+second3=$(echo ${json} | cut -d " " -f7 | cut -d "/" -f1)
 
 
 for var in $second1 $second2 $second3 
-do
-    echo $var
-    if [ -n ${var} ]; then
-        result=$(kill -9 ${var})
-        echo process is killed.
-    else
-        echo running process not found.
-    fi
-done
+    do
+        echo $var
+        if [ -n ${var} ]; then
+            result=$(kill -9 ${var})
+            echo process is killed.
+        else
+            echo running process not found.
+        fi
+    done
